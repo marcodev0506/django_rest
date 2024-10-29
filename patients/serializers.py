855e327c3a4fd8a535c9a8c476e70745f8ad7fbb
Marco Ocanto <mocanto@cnti.gob.ve>
2024-10-29 16:22:28 -0400

    .

--- patients/serializers.py
+++ patients/serializers.py
@@ -2,10 +2,12 @@ from rest_framework import serializers
 
 from .models import Patient, Insurance, MedicalRecord
 from bookings.serializers import AppointmentSerializer
+from datetime import date
 
 
 class PatientSerializer(serializers.ModelSerializer):
     appointments = AppointmentSerializer(many=True, read_only=True)# read_only es para que sea solo lectura
+    age = serializers.SerializerMethodField()
 
     class Meta:
         model = Patient
@@ -13,14 +15,19 @@ class PatientSerializer(serializers.ModelSerializer):
             'id',
             'first_name',
             'last_name',
+            'age',
             'date_of_birth',
             'contact_number',
             'email',
             'address',
             'medical_history',
             'appointments',
-        ]
 
+        ]
+    def get_age(self, obj):
+        age_td = date.today() - obj.date_of_birth
+        years = age_td.days // 365
+        return f"{years} años"
 
 class InsuranceSerializer(serializers.ModelSerializer):
     class Meta:
